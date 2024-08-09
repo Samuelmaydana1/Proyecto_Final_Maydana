0e2899b61afcc6c171226d749d0687c597333221
Samuel M <samuelmaydana1@gmail.com>
2024-08-08 23:41:52 -0300

    Arreglamos imports en AppSamuel/views.py

--- AppSamuel/views.py
+++ AppSamuel/views.py
@@ -1,10 +1,8 @@
-from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.urls import reverse_lazy
 from AppSamuel.models import Autor, Libro, Categoria
